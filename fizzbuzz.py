ac6158b325f6063d7c03f64c9cbd4bbaaf4f2e24
Ethan Koen <epkknd@umsystem.edu>
2023-03-10 14:45:39 -0600

    Made code readable

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,4 @@
+# Looping through every number from 1-100
 for x in range(1,101):
+    # ternary operator to handle printing "Fizz", "Buzz" or "FizzBuzz" if x is a multiple of 3 or 5
     print("FizzBuzz") if x % 3 == 0 and x % 5 == 0 else print("Fizz") if x % 3 == 0 else print("Buzz") if x % 5 == 0 else print(x)
