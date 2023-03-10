110600589f449ccfddb3b44a3ad7f411d7b16867
Ethan Koen <epkknd@umsystem.edu>
2023-03-10 14:41:45 -0600

    Made code more efficient

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,2 @@
 for x in range(1,101):
-    if x % 3 == 0 and x % 5 == 0:
-        print("FizzBuzz")
-    elif x % 3 == 0:
-        print("Fizz")
-    elif x % 5 == 0:
-        print("Buzz")
-    else:
-        print(x)
+    print("FizzBuzz") if x % 3 == 0 and x % 5 == 0 else print("Fizz") if x % 3 == 0 else print("Buzz") if x % 5 == 0 else print(x)
